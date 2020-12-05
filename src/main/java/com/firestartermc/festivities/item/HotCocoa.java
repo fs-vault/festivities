f3cb5cfe074398f535cdbbdda4becf000f9e6bf1
Mykyta Komarnytskyy <nkomarn@hotmail.com>
2020-12-05 12:59:04 -0800

    Tweak hot cocoa behavior

--- src/main/java/com/firestartermc/festivities/item/HotCocoa.java
+++ src/main/java/com/firestartermc/festivities/item/HotCocoa.java
@@ -5,7 +5,9 @@ import com.firestartermc.festivities.api.ItemArchetype;
 import com.firestartermc.kerosene.item.ItemBuilder;
 import com.firestartermc.kerosene.item.SkullBuilder;
 import com.firestartermc.kerosene.util.MessageUtils;
+import com.firestartermc.kerosene.util.PlayerUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Sound;
@@ -36,33 +38,29 @@ public class HotCocoa extends ItemArchetype implements Listener {
     private final NamespacedKey cocoaPowderKey;
     private final ItemStack cocoaPowder;
 
-    private final String cocoaPowerName = "&#633708&lCocoa Powder";
-
     public HotCocoa() {
         super("hot_cocoa_cup", "Hot Cocoa Cup");
 
         this.hotCocoaKey = new NamespacedKey(getId(), "hot_cocoa");
         this.hotCocoa = new SkullBuilder()
-                .name("&#de4721&lHot &#593413&lCocoa")
+                .name("&#e6d0ba&lHOT COCOA")
                 .lore(
-                        "&f&lMakes you feel all",
-                        "&f&lwarm and fuzzy inside",
-                        "",
-                        "&8(Christmas 2020)"
+                        "&fMakes you feel all warm and",
+                        "&ffuzzy inside. Quite a nice drink.",
+                        "&7&o(best before 12/25/20)"
                 )
                 .texture("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZmIyOGFhMzAzYjRjYWU4NzdkN2M2ZDI3YWE4Mzc2MWEyNjkxZjlkNzhhM2Q5Mjg5NTMwY2E2YWJiNWZkZjRjIn19fQ==")
-                .persistData(hotCocoaKey, PersistentDataType.BYTE, (byte)1)
+                .persistData(hotCocoaKey, PersistentDataType.BYTE, (byte) 1)
                 .build();
 
         this.cocoaPowderKey = new NamespacedKey(getId(), "cocoa_powder");
         this.cocoaPowder = ItemBuilder.of(Material.BROWN_DYE)
-                .name(cocoaPowerName)
+                .name("&#966d42&lCocoa Powder")
                 .lore(
-                        "&7Way to bitter to use on it's own.",
-                        "",
-                        "&8(Christmas 2020)"
+                        "&7Way too bitter to use on its own.",
+                        "&7Needs some added milk and sugar."
                 )
-                .persistData(cocoaPowderKey, PersistentDataType.BYTE, (byte)1)
+                .persistData(cocoaPowderKey, PersistentDataType.BYTE, (byte) 1)
                 .build();
     }
 
@@ -71,17 +69,22 @@ public class HotCocoa extends ItemArchetype implements Listener {
         return new SkullBuilder()
                 .name("&f&lEmpty Cup")
                 .lore(
-                        "&fMake some &#593413&lHot Cocoa&r&f!",
-                        "&r",
-                        "&7- &#fafafaMilk Bucket",
-                        "&7- " + cocoaPowerName + " &7(x2)",
-                        "&7- &#fafafaSugar &7(x2)",
-                        "",
-                        "&8(Christmas 2020)"
+                        "&fMake some Hot Cocoa! &fIngredients:",
+                        "&7- &#e6d0baMilk Bucket",
+                        "&7- &#e6d0baCocoa Powder &7(x2)",
+                        "&7- &#e6d0baSugar &7(x2)"
                 )
                 .texture("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZmI4MDk4ODlkYTM2YWRmNTYxODU4MWJiZjdiNjZkMmQ4ODM5ZTJlYjcyNTRjMzMzMmU0ZjNhMjMwZmEifX19")
                 .persistData(TYPE_KEY, PersistentDataType.STRING, getId())
                 .build();
+    }
+
+    @EventHandler
+    public void onPlaceCup(PlayerInteractEvent event) {
+        var player = event.getPlayer();
+        if (matches(player.getInventory().getItemInMainHand()) || matches(player.getInventory().getItemInOffHand())) {
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler
@@ -96,14 +99,21 @@ public class HotCocoa extends ItemArchetype implements Listener {
             return;
 
         event.setCancelled(true);
-        player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 6000, 1));
-        player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 6000, 2));
-        player.playSound(player.getLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0f, 1.0f);
+        item.subtract(1);
 
-        if (item.getAmount() > 1) {
-            item.setAmount(item.getAmount() - 1);
-        } else {
-            player.getInventory().setItemInMainHand(ItemBuilder.of(Material.AIR).build());
+        for (int i = 0; i < 3; i++) {
+            final int sip = i;
+            Bukkit.getScheduler().runTaskLater(Festivities.INSTANCE, () -> {
+                player.playSound(player.getLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0f, 1.0f);
+                if (sip == 2) {
+                    player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_BURP, 1.0f, 1.0f);
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 6000, 1));
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 6000, 3));
+                    player.setFoodLevel(Math.min(player.getFoodLevel() + 8, 20));
+                    player.setSaturation(Math.min(player.getSaturation() + 8, 20));
+                    PlayerUtils.giveOrDropItem(player, getItem());
+                }
+            }, 4L * sip);
         }
     }
 
@@ -116,13 +126,19 @@ public class HotCocoa extends ItemArchetype implements Listener {
             return;
         }
 
+        if (!hasEmptyCup(player)) {
+            return;
+        }
+
         if (!event.hasBlock() || event.getClickedBlock().getType() != Material.GRINDSTONE) {
             return;
         }
 
         event.setCancelled(true);
-        player.getInventory().setItemInMainHand(item.add(-1));
-        player.getInventory().addItem(this.cocoaPowder);
+        player.getInventory().setItemInMainHand(item.subtract(1));
+        player.playSound(event.getClickedBlock().getLocation(), Sound.BLOCK_GRINDSTONE_USE, 1.0f, 1.0f);
+        player.playSound(player.getLocation(), Sound.ENTITY_CHICKEN_EGG, 0.5f, 1.0f);
+        PlayerUtils.giveOrDropItem(player, cocoaPowder);
 
         checkAndCraft(player);
     }
@@ -134,12 +150,15 @@ public class HotCocoa extends ItemArchetype implements Listener {
         if (item.getType() != Material.COCOA_BEANS)
             return;
 
+        if (!hasEmptyCup(event.getPlayer())) {
+            return;
+        }
+
         var meta = item.getItemMeta();
+        meta.setDisplayName(MessageUtils.formatColors("&#966d42&lCococa Beans", true));
         meta.setLore(Arrays.asList(
-                MessageUtils.formatColors("&7Can be grinded to " + cocoaPowerName, true),
-                MessageUtils.formatColors("&7by right clicking on a grindstone.", false),
-                MessageUtils.formatColors("&r", false),
-                MessageUtils.formatColors("&8(Christmas 2020)", false)
+                ChatColor.GRAY + "Can be ground down to cocoa",
+                ChatColor.GRAY + "powder using a grindstone."
         ));
         item.setItemMeta(meta);
     }
@@ -197,8 +216,8 @@ public class HotCocoa extends ItemArchetype implements Listener {
         }
 
         player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1.0f, 1.0f);
-        inventory.addItem(this.hotCocoa);
-        player.sendMessage(MessageUtils.formatColors("&#de4721&lHot &#593413&lCocoa: &#cd91ffYou made some Hot Cocoa. Enjoy!", true));
+        PlayerUtils.giveOrDropItem(player, hotCocoa);
+        player.sendMessage(MessageUtils.formatColors("&#e6d0ba&lHOT COCOA: &fYou made some hot cocoa! Enjoy :>", true));
     }
 
     private boolean takeRequiredComponents(Inventory inventory) {
@@ -260,6 +279,16 @@ public class HotCocoa extends ItemArchetype implements Listener {
             cocoaPowderToRemove = cocoaPowderToRemove - brownDyeStack.getAmount();
             inventory.remove(brownDyeStack);
         }
+    }
+
+    private boolean hasEmptyCup(Player player) {
+        for (var item : player.getInventory().getContents()) {
+            if (isEmptyCup(item)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private boolean isEmptyCup(ItemStack stack) {
